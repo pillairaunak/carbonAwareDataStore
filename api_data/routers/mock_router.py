8dfa13fe62c15e6954360a8c961e4b8ec42e2211
Arka Mitra <thearkamitra@gmail.com>
2025-05-23 11:41:00 +0200

    Real data for carbon

--- api_data/routers/mock_router.py
+++ api_data/routers/mock_router.py
@@ -4,14 +4,29 @@ from schemas.mock_schema import MockData
 from threading import Thread
 import time
 import random
+import requests
 
 router = APIRouter()
 
 mock_data = MockData(pricing_data=0.0, forecasting_data=0.0, carbon_data=0.0)
 
+def get_present_pricing_data():
+    url = "https://api.electricitymap.org/v3/carbon-intensity/latest?zone=DE"
+    headers = {"auth-token": "RZ3Zp595HbEVg9wlGXPa"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
 def update_data():
     while True:
-        mock_data.pricing_data = random.randint(1, 100) + random.random()
+        try:
+            data = get_present_pricing_data()
+            if data:
+                mock_data.pricing_data = data['carbonIntensity']
+        except Exception as e:
+            pass
         mock_data.forecasting_data = random.randint(1, 100) + random.random()
         mock_data.carbon_data = random.randint(1, 100) + random.random()
         time.sleep(60)
